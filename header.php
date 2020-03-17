8086f61ecfc9944979d7986692d6129e8f57c585
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-03-17 15:47:18 +0800

    Update header.php

--- header.php
+++ header.php
@@ -2,25 +2,34 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title><?php
+		      if (function_exists('is_tag') && is_tag()) {
+		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+		      elseif (is_archive()) {
+		         wp_title(''); echo ' Archive - '; }
+		      elseif (is_search()) {
+		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		         wp_title(''); echo ' - '; }
+		      elseif (is_404()) {
+		         echo 'Not Found - '; }
+		      if (is_home()) {
+		         bloginfo('name'); echo ' - '; bloginfo('description'); }
+		      else {
+		          bloginfo('name'); }
+		      if ($paged>1) {
+		         echo ' - page '. $paged; }
+		   ?>
+    </title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    
-    <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="responsive.css"/>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;800&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
 </head>
 
 <body>
@@ -51,81 +60,3 @@
         <h1>Games Design for the future
         </h1>
     </header>
-
-    <section class="container-fluid aboutbg text-center">
-
-        <div class="container">
-            <h2>GAMES &amp; LEVEL DESIGNER</h2>
-            <p class="particle">...and particle wizard</p>
-            <p class="about-text">My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments.
-                I always tell a story.</p>
-            <div class="blocker"></div>
-
-        </div><!--        CONTAINER-->
-
-    </section>
-
-    <section class="container-fluid articlesbg">
-        <div class="container">
-            <div class="row article-title text-center">
-
-                <h3>LATEST ARTICLES</h3>
-            </div><!--        row-->
-
-            <div class="col-md-4 article-cent">
-                <img class="img-responsive" alt="portrait" src="images/portrait.jpg">
-                <h4>A PORTRAIT</h4>
-                <p class="date">12 December 2020</p>
-                <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
-                </p>
-                <p class="readmore">CONTINUE READING</p>
-            </div>
-
-            <div class="col-md-4 article-cent">
-                <img class="img-responsive" alt="model" src="images/model.jpg">
-                <h4>A MODEL</h4>
-                <p class="date">Friday, November 10. 2019</p>
-                <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
-                </p>
-                <p class="readmore">CONTINUE READING</p>
-            </div>
-
-            <div class="col-md-4 article-cent">
-                <img class="img-responsive" alt="games design" src="images/Games_design.jpg">
-                <h4>GAMES DESIGN</h4>
-                <p class="date">Friday, November 10. 2019</p>
-                <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
-                </p>
-                <p class="readmore">CONTINUE READING</p>
-            </div>
-
-        </div><!--    container-->
-
-
-
-    </section><!--    container-fluid-->
-
-    <footer class="container-fluid">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center footer-content">
-                    <p>JOSH WHITKIN</p>
-                    <img src="images/twitter.jpg" alt="twitter">
-                    <img src="images/connect.jpg" alt="connect">
-                </div>
-                <div class="col-md-6 text-center footer-content">
-                    <p>Terms &amp; Conditions</p>
-                    <img src="images/LinkedIn.jpg" alt="LinkedIn">
-                    <img src="images/facebook.jpg" alt="Facebook">
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-</body>
-
-</html>
