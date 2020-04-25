c28e0c166e56266f53d7c5063283f248937f1c31
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-04-25 22:51:29 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -8,6 +8,8 @@
 
     <div class="container">
         <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+        <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+        <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
 
         <div class="blocker"></div>
 
@@ -23,16 +25,16 @@
         </div><!--        row-->
 
         <div class="col-md-4 article-cent">
-            
+
 
         </div>
 
         <div class="col-md-4 article-cent">
-           
+
         </div>
 
         <div class="col-md-4 article-cent">
-            
+
 
         </div>
 
