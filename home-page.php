8356050270cba990471273b8f12bde5f1dad1bed
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-04-25 22:34:53 +0800

    Create home-page.php

---
+++ home-page.php
@@ -0,0 +1,44 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+<section class="container-fluid aboutbg text-center">
+
+    <div class="container">
+        
+
+        <div class="blocker"></div>
+
+    </div><!--        CONTAINER-->
+
+</section>
+
+<section class="container-fluid articlesbg">
+    <div class="container">
+        <div class="row article-title text-center">
+
+            <h3>LATEST ARTICLES</h3>
+        </div><!--        row-->
+
+        <div class="col-md-4 article-cent">
+            
+
+        </div>
+
+        <div class="col-md-4 article-cent">
+           
+        </div>
+
+        <div class="col-md-4 article-cent">
+            
+
+        </div>
+
+    </div><!--    container-->
+
+
+
+</section><!--    container-fluid-->
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
