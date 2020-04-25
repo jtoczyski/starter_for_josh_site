9a0345743ed3363026f96eaca089f199be79193d
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-04-25 21:08:40 +0800

    Update small_header.php

--- small_header.php
+++ small_header.php
@@ -33,7 +33,7 @@
 </head>
 
 <body>
-    <header class="container-fluid small_herobg">
+    <header class="container-fluid herobg">
         <nav class="navbar">
             <div class="container-fluid">
                 <div class="navbar-header">
