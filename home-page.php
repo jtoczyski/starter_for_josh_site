b875035a3f5f8bd94af88b92d74312018c6ebfa0
jtoczyski <49478218+jtoczyski@users.noreply.github.com>
2020-04-25 23:42:41 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -27,24 +27,24 @@
 
         <div class="col-md-4 article-cent">
             <img class="img-responsive" alt=“portait” src="<?php the_field('articleimage'); ?>">
-            <h4><?php the_field('articletitle'); ?></h4>
-            <p><?php the_field('articleexcerpt'); ?> </p>
-            <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
+            <h4><?php the_field('articletitle1'); ?></h4>
+            <p><?php the_field('articleexcerpt1'); ?> </p>
+            <a href="<?php the_field('readmore1'); ?>" class="readmore">CONTINUE READING</a>
 
         </div>
 
         <div class="col-md-4 article-cent">
             <img class="img-responsive" alt=“model” src="<?php the_field('articleimage'); ?>">
-            <h4><?php the_field('articletitle'); ?></h4>
-            <p><?php the_field('articleexcerpt'); ?> </p>
-            <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
+            <h4><?php the_field('articletitle2'); ?></h4>
+            <p><?php the_field('articleexcerpt2'); ?> </p>
+            <a href="<?php the_field('readmore2'); ?>" class="readmore">CONTINUE READING</a>
         </div>
 
         <div class="col-md-4 article-cent">
             <img class="img-responsive" alt=“gamesdesign” src="<?php the_field('articleimage'); ?>">
-            <h4><?php the_field('articletitle'); ?></h4>
-            <p><?php the_field('articleexcerpt'); ?> </p>
-            <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
+            <h4><?php the_field('articletitle3'); ?></h4>
+            <p><?php the_field('articleexcerpt3'); ?> </p>
+            <a href="<?php the_field('readmore3'); ?>" class="readmore">CONTINUE READING</a>
 
         </div>
 
